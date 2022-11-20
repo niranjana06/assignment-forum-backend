42605710878bd2eac6b97aeafc0451162429f8b7
Niranjana <niranjana.solomoit@gmail.com>
2022-11-20 09:47:51 +0530

    Auth controller update

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -20,4 +20,26 @@ class ApiController extends Controller
         ], $statusCode, $headers);
     }
 
+    protected function respondError($message, $status, $code = '')
+    {
+        return $this->respond([
+            'success' => false,
+            'error' => [
+                'message' => $message,
+                'status' => $status,
+                'code' => $code
+            ]
+        ], $status);
+    }
+
+    protected function respondUnauthorized($message = 'Unauthorized')
+    {
+        return $this->respondError($message, 401);
+    }
+
+    protected function respondInternalError($message = 'Internal Error')
+    {
+        return $this->respondError($message, 500);
+    }
+
 }
